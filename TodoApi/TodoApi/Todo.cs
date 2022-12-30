459c7ed80c4497c1a992d1627f6fa0e64b8c0c7e
Charan Karthik <secharank@gmail.com>
2022-12-29 18:43:34 -0800

    first commit

---
+++ TodoApi/TodoApi/Todo.cs
@@ -0,0 +1,7 @@
+﻿// The model for a to-do task
+class Todo
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
---
+++ TodoApi/TodoApi/TodoDb.cs
@@ -0,0 +1,11 @@
+﻿// Define database context for the to-do model
+
+using Microsoft.EntityFrameworkCore;
+
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
